02d78e63cb917d866128f920ce5061d2ed224f18
svanhesteren <s.vanhesteren@gmail.com>
2018-01-29 17:38:19 +0100

    Fix user reference

--- db/migrate/20180129163310_create_posts.rb
+++ db/migrate/20180129163310_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.text :content
-      t.references :user_id, foreign_key: true
+      t.references :user, foreign_key: true
       t.string :link
       t.string :images
       t.string :video
