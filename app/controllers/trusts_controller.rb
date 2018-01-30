a7a18be6a205eb444b0fe8bf98f7c321cca499b0
svanhesteren <s.vanhesteren@gmail.com>
2018-01-30 16:37:13 +0100

    Add trust create action

--- app/controllers/trusts_controller.rb
+++ app/controllers/trusts_controller.rb
@@ -1,3 +1,21 @@
 class TrustsController < ApplicationController
 
+  def create
+    trust = Trust.new(trust_params)
+
+    if trust.save
+      render notice: "Trust created",json: trust.as_json
+    else
+      render notice: "Trust not created", json: trust.errors.full_messages
+    end
+
+  end
+
+
+
+
+  def trust_params
+    params.require(:trust).permit(:source_id, :screenshot, :link, :post_id , :user_id)
+  end
+
 end
