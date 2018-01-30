d0ddb6bb6b7628dcb022bee043f83dfcf90a134d
svanhesteren <s.vanhesteren@gmail.com>
2018-01-30 16:46:59 +0100

    Add custom sessionsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations', sessions: 'sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "posts#index"
 
