8449ff82aa3ff4e0dcdd58c155416a77254e7321
Abohte <adinda.bohte@gmail.com>
2018-01-30 10:54:15 +0100

    Add validation tests for post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:content) }
+    it { is_expected.to validate_presence_of(:link) }
+    it { is_expected.to validate_presence_of(:images)}
+    it { is_expected.to validate_presence_of(:video)}
+    it { is_expected.to validate_presence_of(:is_spam)}
+    it { is_expected.to validate_length_of(:content).is_at_most(500) }
+  end
+
 end
