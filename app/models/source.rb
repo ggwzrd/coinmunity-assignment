c39babe4de09b45906de530babbe743ad4386b7f
Abohte <adinda.bohte@gmail.com>
2018-01-30 13:24:59 +0100

    Add validations for source, tag, trust, report, profile

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,3 +1,11 @@
 class Source < ApplicationRecord
   has_many :trusts
+
+  validates :name, presence: true
+  validates :authenticity, presence: true
+  validates :logo, presence: true
+  validates :description, presence: true
+  validates :domain, presence: true
+  validates :secure_connection, presence: true
+  validates :verified, presence: true
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,7 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :posts
+
+  validates :name, presence: true
+  validates :description, presence: true
+
 end
