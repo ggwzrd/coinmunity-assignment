0b077d12951fb711fbaac15273028e303d8e5b50
svanhesteren <s.vanhesteren@gmail.com>
2018-01-30 17:40:13 +0100

    registrations changes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,26 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  respond_to :json
+
+  def create
+
+    user = User.new(sign_up_params)
+
+    if user.save
+      render json: user.as_json, status: 201
+    else
+      render json: user.errors.full_messages, status: 422, notice: "User not created"
+    end
+
+  end
+
   private
 
   def sign_up_params
-    params.require(:user).permit(:truthiness, :silenced, :email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:truthiness, :silenced, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
   end
 end
