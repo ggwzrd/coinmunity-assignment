2b0b7f312246e3272abee8daf4d3f3b6fb18c188
Abohte <adinda.bohte@gmail.com>
2018-02-01 17:59:05 +0100

    Add trustiness bonus to filled-in fields in profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,8 @@
 class ProfilesController < BaseController
+  FIRST_NAME_TRUSTINESS_BONUS = 1
+  LAST_NAME_TRUSTINESS_BONUS = 1
+  PICTURE_TRUSTINESS_BONUS = 2
+  BIO_TRUSTINESS_BONUS = 1
 
   def create
     temp_params = profiles_params
@@ -6,6 +10,10 @@ class ProfilesController < BaseController
     profile = Profile.new(profile_params)
 
     if user.save
+      @user.update_trustiness(FIRST_NAME_TRUSTINESS_BONUS) if profile.first_name
+      @user.update_trustiness(LAST_NAME_TRUSTINESS_BONUS) if profile.last_name
+      @user.update_trustiness(PICTURE_TRUSTINESS_BONUS) if profile.picture
+      @user.update_trustiness(BIO_TRUSTINESS_BONUS) if profile.bio
       render json: user.as_json, status: 201
     else
       render json: user.errors.full_messages, status: 422, notice: "Profile not created"
