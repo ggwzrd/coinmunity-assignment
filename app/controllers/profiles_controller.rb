3b30627def9e1cba3dad8fff9264cf3d0f12666e
Abohte <adinda.bohte@gmail.com>
2018-02-01 17:28:18 +0100

    Add attributes to profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,16 @@
 class ProfilesController < BaseController
+
+  def create
+    profile = Profile.new(profile_params)
+
+    if user.save
+      render json: user.as_json, status: 201
+    else
+      render json: user.errors.full_messages, status: 422, notice: "Profile not created"
+    end
+  end
+
+  private
+
+  params.require(:profiles).permit(:nickname, :first_name, :last_name, :picture, :bio)
 end
--- db/migrate/20180129170944_create_profiles.rb
+++ db/migrate/20180129170944_create_profiles.rb
@@ -1,9 +1,11 @@
 class CreateProfiles < ActiveRecord::Migration[5.1]
   def change
     create_table :profiles do |t|
+      t.string :last_name
       t.string :first_name
       t.string :last_name
       t.string :picture
+      t.text :bio
       t.references :user, foreign_key: true
 
       t.timestamps
