b96d5dfbcf3719cf0a585bf6c9e149ff36d5d5ae
Abohte <adinda.bohte@gmail.com>
2018-02-06 15:13:06 +0100

    Add tests for comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:text) }
+  end
+
+  describe "associations" do
+    describe "association with user" do
+      it { is_expected.to belong_to(:user) }
+    end
+
+    describe "association with post" do
+      it { is_expected.to belong_to(:post) }
+    end
+  end
 end
